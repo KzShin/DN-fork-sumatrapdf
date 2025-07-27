116536ce8862c012274ddd3ad38d70d5301177e3
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2025-07-27 20:11:09 +0900

    新しいタブで開く際にページ情報をコピー

--- src/SumatraPDF.cpp
+++ src/SumatraPDF.cpp
@@ -3174,12 +3174,20 @@ void DuplicateTabInNewTab(WindowTab* tab) {
     if (!path) {
         return;
     }
+    TabState* ts = CopyTabState(tab);
     MainWindow* win = tab->win;
     LoadArgs args(path, win);
     args.forceReuse = false; // open in a new tab in the same window
     args.showWin = true;
     args.noPlaceWindow = true;
     LoadDocument(&args, false, false);
+    if (ts) {
+        WindowTab* newTab = win->CurrentTab();
+        if (newTab) {
+            SetTabState(newTab, ts);
+        }
+        //FreeStruct(&gTabStateInfo, ts);
+    }
 }
 
 // create a new window and load currently shown document into it
